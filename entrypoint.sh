441d3ff779540efd83f34578ec5b25b3d877c65a
beermix <27rurk@gmail.com>
2020-01-19 03:36:30 +0300

    autocommit on change

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 SHADOWSOCKS_PORT=${SHADOWSOCKS_PORT:-"443"}
 SHADOWSOCKS_PASSWORD=${SHADOWSOCKS_PASSWORD:-"asdewq123"}
-SHADOWSOCKS_CRYPTO=${SHADOWSOCKS_CRYPTO:-"chacha20-ietf-poly1305"}
+SHADOWSOCKS_CRYPTO=${SHADOWSOCKS_CRYPTO:-"chacha20"}
 OBFS_PORT=${OBFS_PORT:-"993"}
 OBFS_PROTOCOL=${OBFS_PROTOCOL:-"tls"}
 KCPTUN_PORT=${KCPTUN_PORT:-"53"}
 KCPTUN_MODE=${KCPTUN_MODE:-"fast2"}
 KCPTUN_KEY=${KCPTUN_KEY:-"asdewq123"}
-KCPTUN_CRYPTO=${KCPTUN_CRYPTO:-"salsa20"}
+KCPTUN_CRYPTO=${KCPTUN_CRYPTO:-"chacha20"}
 
 if [ -z $KCPTUN_OVER_OBFS ]; then
   KCPTUN_TARGET_PORT=$SHADOWSOCKS_PORT
@@ -23,4 +23,4 @@ echo "Starting Obfs Server on port $OBFS_PORT over $SHADOWSOCKS_PORT with protoc
 obfs-server -r "127.0.0.1:$SHADOWSOCKS_PORT" -p "$OBFS_PORT" --obfs "$OBFS_PROTOCOL" &
 
 echo "Starting Kcptun Server on udp port $KCPTUN_PORT over $KCPTUN_TARGET_PORT with crypto $KCPTUN_CRYPTO..."
-kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO" --nocomp
+kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO" --mtu 1350 --sndwnd 1024 --rcvwnd 1024
