4b596ce4e813ca4d244dafa8888bcb3783e83cf0
beermix <27rurk@gmail.com>
2020-03-04 20:47:03 +0300

    autocommit on change

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ SHADOWSOCKS_PASSWORD=${SHADOWSOCKS_PASSWORD:-"asdewq123"}
 SHADOWSOCKS_CRYPTO=${SHADOWSOCKS_CRYPTO:-"chacha20"}
 OBFS_PORT=${OBFS_PORT:-"993"}
 OBFS_PROTOCOL=${OBFS_PROTOCOL:-"tls"}
-KCPTUN_PORT=${KCPTUN_PORT:-"533"}
-KCPTUN_MODE=${KCPTUN_MODE:-"fast2"}
+KCPTUN_PORT=${KCPTUN_PORT:-"53"}
+KCPTUN_MODE=${KCPTUN_MODE:-"fast"}
 KCPTUN_KEY=${KCPTUN_KEY:-"asdewq123"}
 KCPTUN_CRYPTO=${KCPTUN_CRYPTO:-"chacha20"}
 
@@ -17,11 +17,10 @@ else
 fi
 
 echo "Starting Shadowsocks Server on port $SHADOWSOCKS_PORT with crypto $SHADOWSOCKS_CRYPTO..."
-ss-server -s 0.0.0.0 -p "$SHADOWSOCKS_PORT" -k "$SHADOWSOCKS_PASSWORD" -m "$SHADOWSOCKS_CRYPTO" --fast-open --reuse-port --no-delay -u -d 1.0.0.1 &
+ss-server -s 0.0.0.0 -p "$SHADOWSOCKS_PORT" -k "$SHADOWSOCKS_PASSWORD" -m "$SHADOWSOCKS_CRYPTO" --fast-open -d 1.0.0.1 &
 
 echo "Starting Obfs Server on port $OBFS_PORT over $SHADOWSOCKS_PORT with protocol $OBFS_PROTOCOL..."
 obfs-server -r "127.0.0.1:$SHADOWSOCKS_PORT" -p "$OBFS_PORT" --obfs "$OBFS_PROTOCOL" --fast-open
 
 echo "Starting Kcptun Server on udp port $KCPTUN_PORT over $KCPTUN_TARGET_PORT with crypto $KCPTUN_CRYPTO..."
-kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO" --mtu 512 --datashard 70 --parityshard 30 --sndwnd 1024 --dscp 46
-
+kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO"  --mtu 1350 --sndwnd 1024 --rcvwnd 1024
