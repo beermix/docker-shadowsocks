9e53f73507d617a87b86b4797f879a480fde5028
beermix <27rurk@gmail.com>
2020-03-04 20:39:40 +0300

    autocommit on change

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,5 @@ echo "Starting Obfs Server on port $OBFS_PORT over $SHADOWSOCKS_PORT with protoc
 obfs-server -r "127.0.0.1:$SHADOWSOCKS_PORT" -p "$OBFS_PORT" --obfs "$OBFS_PROTOCOL" --fast-open
 
 echo "Starting Kcptun Server on udp port $KCPTUN_PORT over $KCPTUN_TARGET_PORT with crypto $KCPTUN_CRYPTO..."
-kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO"  --mtu 1350 --sndwnd 1024 --rcvwnd 1024
+kcptun-server --target "127.0.0.1:$KCPTUN_TARGET_PORT" --listen ":$KCPTUN_PORT" --mode "$KCPTUN_MODE" --key "$KCPTUN_KEY" --crypt "$KCPTUN_CRYPTO" --mtu 512 --datashard 70 --parityshard 30 --sndwnd 1024 --dscp 46
+
